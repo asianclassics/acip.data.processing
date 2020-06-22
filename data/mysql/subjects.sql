b7769de8fea17334363070e97e3f9f99acfa7834
joelcrawford <joelrootcrawford@gmail.com>
2020-06-22 15:21:59 -0700

    added data dir to gitignore

--- data/mysql/subjects.sql
+++
@@ -1,9 +0,0 @@
-SELECT 'tibEntry', 'engEntry', 'sktEntry', 'newTibEntry', 'newEngEntry', 'newSktEntry', 'recID'
-UNION ALL
-SELECT tibEntry, engEntry, sktEntry, newTibEntry, newEngEntry, newSktEntry, recID
-FROM 
-	acipmaintenance.subjitems
-GROUP BY tibEntry, engEntry, sktEntry, newTibEntry, newEngEntry, newSktEntry, recID
--- INTO 
--- 	outfile '/var/lib/mysql-files/etext_subjects_with_headers.csv' 
---     fields terminated by ',' enclosed by '"' lines terminated by '\n';
